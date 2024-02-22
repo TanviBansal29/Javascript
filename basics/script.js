b76f454af2bcfaeb4263503518d143bfd95e18ad
Tanvi Bansal <Tanvi.Bansal@watchguard.com>
2024-02-22 11:06:04 +0530

    Added conditional Statements

--- basics/script.js
+++ basics/script.js
@@ -1,5 +1,5 @@
-document.write("Welcome to my site");
+document.write("Welcome to my site"); //to write on web page
 // addpopup message
 
-alert("hello");
+alert("hello");  // we are on node js not browser so it will throw an error
 console.log("it worked");
--- basics/variables.js
+++ basics/variables.js
@@ -1,16 +1,29 @@
-const accountId = 12345
-let accountEmail = "hello@gmail.com"
-var accountPassword = "hello12@"
-accountCity = "delhi"
+// const accountId = 12345
+// let accountEmail = "hello@gmail.com"
+// var accountPassword = "hello12@"
+// accountCity = "delhi"
 
-let accountState;
-// const means now the value cant be chsnged once initialized
+// let accountState;
+// // const means now the value cant be chsnged once initialized
 
-// accountId = 2   (Cannot be changed now)
+// // accountId = 2   (Cannot be changed now)
 
-accountEmail = "tanvi@gmail.com"
-accountPassword = "21212121"
-accountCity = "noida"
+// accountEmail = "tanvi@gmail.com"
+// accountPassword = "21212121"
+// accountCity = "noida"
 
-console.log(accountId);
-console.table([accountEmail, accountId, accountPassword, accountCity, accountState])
+// console.log(accountId);
+// console.table([accountEmail, accountId, accountPassword, accountCity, accountState])
+
+
+let userOne = {
+    email: "user@gamil.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userOne = { key: "value" }
+
+console.log(userOne)
+console.log(userTwo)
